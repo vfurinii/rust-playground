66d49e64a5eb73506014213b0e48156822e285f6
Vitor Furini <vitorfurini@hotmail.com>
2025-11-11 22:36:57 -0300

    add a hello world project

---
+++ hello_world/target/debug/deps/hello_world-d9ec0ab4b6a1fa79.d
@@ -0,0 +1,5 @@
+E:\workspace\rust-playground\first-project\src\hello_world\target\debug\deps\hello_world-d9ec0ab4b6a1fa79.d: src\main.rs
+
+E:\workspace\rust-playground\first-project\src\hello_world\target\debug\deps\hello_world-d9ec0ab4b6a1fa79.exe: src\main.rs
+
+src\main.rs:
---
+++ todo_cli/target/debug/deps/todo_cli.d
@@ -0,0 +1,5 @@
+E:\workspace\rust-playground\todo_cli\target\debug\deps\todo_cli.d: src\main.rs
+
+E:\workspace\rust-playground\todo_cli\target\debug\deps\todo_cli.exe: src\main.rs
+
+src\main.rs:

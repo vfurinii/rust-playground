d11f6d43fe6fb510976bc43c284e527219aa66b7
Vitor Furini <vitorfurini@hotmail.com>
2025-11-13 23:11:14 -0300

    creating a list and populating then

---
+++ hello_world/target/debug/deps/hello_world-21e1c7e0004a3f03.d
@@ -0,0 +1,5 @@
+E:\workspace\rust-playground\hello_world\target\debug\deps\hello_world-21e1c7e0004a3f03.d: src\main.rs
+
+E:\workspace\rust-playground\hello_world\target\debug\deps\libhello_world-21e1c7e0004a3f03.rmeta: src\main.rs
+
+src\main.rs:
